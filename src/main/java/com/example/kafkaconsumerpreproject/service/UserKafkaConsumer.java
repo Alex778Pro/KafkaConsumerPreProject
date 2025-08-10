8d8a0dd97446c02a95393b04ffc9d106f61dabc0
Alex778Pro <aleksander.terehin2014@yandex.ru>
2025-08-10 19:27:50 +0300

    Правки

--- src/main/java/com/example/kafkaconsumerpreproject/service/UserKafkaConsumer.java
+++ src/main/java/com/example/kafkaconsumerpreproject/service/UserKafkaConsumer.java
@@ -1,14 +1,17 @@
 package com.example.kafkaconsumerpreproject.service;
 
 import com.example.kafkaconsumerpreproject.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserKafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(UserKafkaConsumer.class);
+
     @KafkaListener(topics = "test_topic")
     public void consumeUser(User user) {
-        System.out.println("User - " + user);
-
+        log.info("User - " + user);
     }
 }
